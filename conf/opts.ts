d395ea826d7466baa599c4ada1a8a489f043baac
Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
2022-09-08 15:01:44 +0300

    conf: include site-specfiic files with VM TAs build config
    The file should define build host and target Linux kernel
    for VMs available on site.
    
    For example:
    export TE_TA_x86_64_debian11_BUILD_HOST=build-x86-64-debian11
    export TE_TA_x86_64_debian11_KERNEL_RELEASE=5.10.0-17-amd64
    
    Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- conf/opts.ts
+++ conf/opts.ts
@@ -2,6 +2,7 @@
 # (c) Copyright 2016 - 2022 Xilinx, Inc. All rights reserved.
 --conf-cs=cs/vswitch-ts.yml
 --script=scripts/ta-def
+--script=env/ta-build
 --script=script.ta-build
 --conf-cs=cs.vm-tmpl.yml
 --script=scripts/defaults
