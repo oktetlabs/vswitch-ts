d286a59b12a54da4d22c315ca5f7b085174b71fb
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2023-08-11 10:16:57 +0300

    scripts: fix variables guessing
    Guessing values of variables like TE_TS_RIGSDIR should be done
    in guess.sh which should be used at the beginning of run.sh,
    like it is done in other test suites.
    Currently it is done after run.sh tries to load grab configuration
    handlers from ts-rigs, and configuration grabbing does not work.
    
    OL-Redmine-Id: 12984
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- scripts/run.sh
+++ scripts/run.sh
@@ -10,6 +10,8 @@
 RUNDIR="$(pwd -P)"
 export TS_TOPDIR="$(cd "$(dirname "$(which "$0")")"/.. ; pwd -P)"
 
+source "$(dirname "$(which "$0")")"/guess.sh
+
 source "${TE_BASE}/scripts/lib"
 source "${TE_BASE}/scripts/lib.grab_cfg"
 
@@ -26,17 +28,6 @@ run_fail() {
     echo $* >&2
     exit 1
 }
-
-if test -z "${TE_BASE}" ; then
-    if test -e dispatcher.sh ; then
-        export TE_BASE="${RUNDIR}"
-    elif test -e "${TS_TOPDIR}/dispatcher.sh" ; then
-        export TE_BASE="${TS_TOPDIR}"
-    else
-        run_fail "Path to TE sources MUST be specified in TE_BASE"
-    fi
-fi
-
 
 usage() {
 cat <<EOF
@@ -103,42 +94,6 @@ done
 
 if test -n "${CFG}" ; then
     process_cfg ${CFG}
-fi
-
-if test -z "${TE_BUILD}" ; then
-    if test "${TS_TOPDIR}" = "${RUNDIR}" ; then
-        TE_BUILD="${TS_TOPDIR}/build"
-        mkdir -p build
-    else
-        TE_BUILD="${RUNDIR}"
-    fi
-    export TE_BUILD
-fi
-
-if test -z "${SF_TS_CONFDIR}" ; then
-    SF_TS_CONFDIR="${TS_TOPDIR}"/../conf
-    if test ! -d "${SF_TS_CONFDIR}" ; then
-        SF_TS_CONFDIR="${TS_TOPDIR}"/../ts-conf
-        if test ! -d "${SF_TS_CONFDIR}" ; then
-            SF_TS_CONFDIR="${TS_TOPDIR}"/../ts_conf
-            if test ! -d "${SF_TS_CONFDIR}" ; then
-                run_fail "Path to shared Xilinx ts-conf MUST be specified in SF_TS_CONFDIR"
-            fi
-        fi
-    fi
-    echo "Guessed SF_TS_CONFDIR=${SF_TS_CONFDIR}"
-    export SF_TS_CONFDIR
-fi
-
-if test -z "${TE_TS_RIGSDIR}" ; then
-    TE_TS_RIGSDIR="${TS_TOPDIR}"/../ts-rigs
-    if [[ -d "${TE_TS_RIGSDIR}" ]] ; then
-        TE_TS_RIGSDIR="$(realpath "${TE_TS_RIGSDIR}")"
-        echo "Guessed TE_TS_RIGSDIR=${TE_TS_RIGSDIR}"
-        export TE_TS_RIGSDIR
-    else
-        unset TE_TS_RIGSDIR
-    fi
 fi
 
 MY_OPTS=
